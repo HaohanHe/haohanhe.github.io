6e821ed7707c338e3e430d183987d03b9cef6032
hsyscn <hehaohan@hsyscn.top>
2024-07-31 08:34:21 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ function scrollFunction() {
         document.getElementsByClassName("navbar")[0].classList.remove("scrolled");  
     }  
 }
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(slideIndex) {
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+    });
+    slides[slideIndex].classList.add('active');
+}
+
+function nextSlide() {
+    currentSlide++;
+    if (currentSlide >= slides.length) {
+        currentSlide = 0;
+    }
+    showSlide(currentSlide);
+}
+
+function startSlideshow() {
+    setInterval(nextSlide, 3000); // 每隔3秒切换一次轮播图
+}
+
+startSlideshow();
